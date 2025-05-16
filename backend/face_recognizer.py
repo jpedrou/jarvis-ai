22b22039a7d7f2ff4915b04a1c92e7f99033d2bc
João Pedro <jpedro.open@gmail.com>
2025-05-16 12:59:00 -0300

    refactor: make funciton async

--- backend/face_recognizer.py
+++ backend/face_recognizer.py
@@ -3,11 +3,13 @@ import time
 import face_recognition
 
 
-def capture_face():
+async def capture_face():
     cap = cv2.VideoCapture(0)
     start_time = time.time()
     while True:
         ret, frame = cap.read()
+        if not ret:
+            break
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         face_locations = face_recognition.face_locations(rgb)
 
@@ -15,8 +17,15 @@ def capture_face():
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.imshow('Video', frame)
 
-        if time.time() - start_time > 15 and len(face_locations) == 1:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        if time.time() - start_time > 6 and len(face_locations) == 1:
             encoding = face_recognition.face_encodings(rgb, face_locations)[0]
             cap.release()
             cv2.destroyAllWindows()
             return encoding
+    
+    cap.release()
+    cv2.destroyAllWindows()
+    return None
